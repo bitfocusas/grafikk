e46e3d41b9c818d72c4e9f5505f287df5749cbee
Håkon Nessjøen <haakon@bitfocus.io>
2020-04-21 17:30:50 +0200

    Test: Update text on event

--- test.ts
+++ test.ts
@@ -15,16 +15,17 @@ var io = require('socket.io')(server);
 
 let currentRenderText = "Abc";
 
-server.listen(serverPort, function () { console.log('server up and running at %s port', serverPort); });
-/* end socket.io server */
+interface testInterface {
+	id: string;
+	physicalW: number;
+	physicalH: number;
+	pixelsW: number;
+	pixelsH: number;
+	mono: boolean;
+	gfx?: Grafikk;
+}
 
-io.on('connection', (socket: { on: (arg0: string, arg1: (text: any) => void) => void; }) => {
-	socket.on('updateText', (text: string) => {
-		currentRenderText = text
-	})
-});
-
-const tests = [
+const tests: testInterface[] = [
 	{ id: 'skaarhojSmall', physicalW: 13.4, physicalH: 7.6, pixelsW: 64, pixelsH: 32, mono: true },
 	{ id: 'skaarhojWide', physicalW: 26.8, physicalH: 7.6, pixelsW: 128, pixelsH: 32, mono: true },
 	{ id: 'streamdeckNormal', physicalW: 14, physicalH: 14, pixelsW: 72, pixelsH: 72, mono: false },
@@ -32,19 +33,34 @@ const tests = [
 	{ id: 'streamdeckMini', physicalW: 14, physicalH: 14, pixelsW: 80, pixelsH: 80, mono: false },
 ]
 
-tests.map(outputSpecification => {
+server.listen(serverPort, function () { console.log('server up and running at %s port', serverPort); });
+/* end socket.io server */
 
-	const gfx = new Grafikk(outputSpecification, (renderResult) => {
-		io.emit('render', outputSpecification.id, outputSpecification, renderResult);
-	});
+tests.forEach(outputSpecification => {
+	outputSpecification.gfx = new Grafikk(outputSpecification, (renderResult) => {
+		io.emit('render', outputSpecification.id, outputSpecification, renderResult)
+	})
+})
 
-	setInterval(() => {
-		gfx.generate({
-			text: currentRenderText
-		})
-	}, 200)
+function update() {
+	tests.forEach(outputSpecification => {
+		if (outputSpecification.gfx) {
+			debug(`Going to generate ${outputSpecification.pixelsW}x${outputSpecification.pixelsH}`)
+			outputSpecification.gfx.generate({
+				text: currentRenderText
+			})
+			debug('done')
+		}
+	})
+}
 
+io.on('connection', (socket: { on: (arg0: string, arg1: (text: any) => void) => void; }) => {
+	update();
 
+	socket.on('updateText', (text: string) => {
+		currentRenderText = text
+		update()
+	})
 })
 
 debug("x")
